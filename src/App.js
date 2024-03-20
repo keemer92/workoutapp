3a75e7a8bbf21458ffaf653e1e2624c78b44e0f6
Waquiem Comar <wcomar92@gmail.com>
2024-03-20 12:10:19 -0400

    fnished the upper body and lower body workout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './gym.jpg';
+import './styles.css';
 
 function App() {
   return (
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="workOut.html"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click here to hit your fitness goals!
         </a>
       </header>
     </div>
